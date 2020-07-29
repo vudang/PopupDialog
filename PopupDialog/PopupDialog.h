4da0c36e1c96882dd2f863fb66ac301034cf49fc
vudang <ngocvu.cse09@gmail.com>
2020-07-29 22:31:46 +0700

    init

---
+++ PopupDialog/PopupDialog.h
@@ -0,0 +1,19 @@
+//
+//  PopupDialog.h
+//  PopupDialog
+//
+//  Created by Vu Dang on 7/29/20.
+//  Copyright © 2020 Vu Dang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PopupDialog.
+FOUNDATION_EXPORT double PopupDialogVersionNumber;
+
+//! Project version string for PopupDialog.
+FOUNDATION_EXPORT const unsigned char PopupDialogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PopupDialog/PublicHeader.h>
+
+
